1f798b094fcffc75f96f18a2927193081eeb48be
Namjuy <najumy1807.2k@gmail.com>
2024-03-06 23:30:05 +0700

    update

--- BA_front-end/src/app/common/components/generic-modal/generic-modal.component.ts
+++ BA_front-end/src/app/common/components/generic-modal/generic-modal.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-generic-modal',
   templateUrl: './generic-modal.component.html',
-  styleUrls: ['./generic-modal.component.scss']
+  styleUrls: ['./generic-modal.component.scss'],
 })
+//1/1/2024
 export class GenericModalComponent implements OnInit {
+  @Input() modalTitle: string = '';
+  @Input() labelItems: any;
+  @Input() form: FormGroup | any;
+  
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+    else{
+      console.log(1);
+      
+    }
   }
-
 }
